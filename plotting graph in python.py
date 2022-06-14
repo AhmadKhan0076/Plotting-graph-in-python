2cb2b622ece18806312179332ff7ee100c0996d8
Ahmad Khan <98169512+AhmadKhan0076@users.noreply.github.com>
2022-06-14 08:23:51 -0700

    Update plotting graph in python.py

--- plotting graph in python.py
+++ plotting graph in python.py
@@ -7,7 +7,7 @@ Created on Wed Mar 16 15:29:17 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
-t=np.linspace(0,40)
+t=np.linspace(0,100)
 d_r=1*t
 d_s=3*(t-30)
 fig,ax=plt.subplots()
@@ -17,8 +17,8 @@ ax.set_xlim(0,50)
 ax.set_ylim(0,50)
 ax.plot(t,d_r,c='red')
 ax.plot(t,d_s,c='black')
-plt.axvline(x=40,color='purple',linestyle="--")
-_= plt.axhline(y=40,color='purple',linestyle="--")
+plt.axvline(x=45,color='purple',linestyle="--")
+_= plt.axhline(y=45,color='purple',linestyle="--")
 
 
 
